48c146caf61ffbe05d0123035cc576eced6a34e7
Claudia Lisboa <ccalumby@gmail.com>
2022-07-07 11:17:45 -0400

    uppdated script function affiche carrousel img

--- script/carrousel.js
+++ script/carrousel.js
@@ -5,7 +5,6 @@
  * 
  */
 
-
  (function(){
     console.log('Début du carrousel')
     let elmGalerie = document.querySelector('.galerie')
@@ -15,13 +14,18 @@
     let elmCarrousel__fermeture = document.querySelector('.carrousel__fermeture')
     let elmCarrousel__radio = document.querySelector('.carrousel__radio')
     console.log(elmGalerieImg[0].getAttribute('src'))
-    let index = 0 // L,index qui permettra d'indexer les images du carrousel et les radios bouton
+    let index = 0 // L'index qui permettra d'indexer les images du carrousel et les radios bouton
+    let ancien_index = -1
+    let compteur =0
     for (const img of elmGalerieImg){
+        img.dataset.index = compteur++
         ajouter_elmImg(img)
         ajouter_elmRadio()
         // écouteur sur la galerie pour ouvrir le carrousel
         img.addEventListener('mousedown', function(){
             elmCarrousel.classList.add('carrousel__ouvrir') 
+            index = this.dataset.index
+            affiche_carrousel_img()
     }) 
     }
 /**
@@ -54,15 +58,25 @@
 
     function ajouter_elmRadio(){
         let elmRadio = document.createElement('input')
-
+        elmRadio.setAttribute('type','radio')
+        elmRadio.setAttribute('name','radImg')
         elmRadio.dataset.index = index
         index = index+1
         elmCarrousel__radio.appendChild(elmRadio)
         /* Écouteur pour sélectionner une nouvelle image */ 
         elmRadio.addEventListener('mousedown', function(){    
-            elmCarrousel__figure.children[this.dataset.index].classList.add('carrousel__figure__img--activer')
+            index = this.dataset.index
+            affiche_carrousel_img()
         })
     }
 
-
+function affiche_carrousel_img()
+{
+    if (ancien_index != -1){
+        elmCarrousel__figure.children[ancien_index].classList.remove('carrousel__figure__img--activer') 
+    }
+    elmCarrousel__figure.children[index].classList.add('carrousel__figure__img--activer')
+    ancien_index = index
+}
+    
 })()
