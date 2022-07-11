b160339a190bd07d7ef6011eee254997aa354849
Claudia Lisboa <ccalumby@gmail.com>
2022-07-11 14:18:41 -0400

    updated nav_sidebar

---
+++ header.php
@@ -0,0 +1,44 @@
+<?php
+ /**
+  * Template header
+  * Ve template est intégré dans tous les modèles de thème
+  * @link : https://developer.wordpress.org/themes/
+  * @package : 31w
+  */
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>  >
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+<body>
+    <section class="site">
+        <header class="site__header">
+            <section class="site__branding">
+                <h1>
+                    <a href="<?= esc_url( home_url( '/' ) ) ?>"><?= bloginfo( 'name' ); ?></a>
+                </h1>
+                <h2>
+                <?= bloginfo( 'description' ); ?>
+                </h2>
+            </section>
+            <nav class="site__navigation">
+            <h4>Menu</h4>
+            </nav>
+        </header>
+        <section class="site__sidebar">
+            <h3>Sidebar</h3>
+            <?php  wp_nav_menu(array(
+                'menu'=>'',
+                'container'=>'nav',
+                'container_class'=>'menu_sidebar',
+                'menu_class'=>'menu__sidebar__ul',
+
+
+            ))  ?>
+        </section>
+    
+
---
+++ readme.md
@@ -0,0 +1,13 @@
+# TP1
+## Thème et plugin  Wordpress
+### Contient 12 commits
+
+[github-page](https://eddytuto.github.io/theme-31w)
+> Le thème utilise une structure de grid pour formater le layout
+
+
+Pour plus d'information sur la conception de thème
+[WP developper guide](https://developper.wordpress.org/theme)
+
+Pour plus d'information sur la structure grid
+[css-tricks/grid](https://css-tricks.com/snippets/css/complete-guide-grid/)
