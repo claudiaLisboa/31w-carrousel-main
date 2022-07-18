18b9b36e833efaee0bad704406ecfe5e44ab564d
Claudia Lisboa <ccalumby@gmail.com>
2022-07-18 18:53:04 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php get_header(); ?> 
-<h1>Le plus beau chien du monde 2022 </h1>
-<?php
-if (have_posts()):
-    while(have_posts()) : the_post(); ?>
-        <h2><?php the_title(); ?></h2>
-    <?php endwhile; ?>
-<?php endif; ?>    
+    <h1>Le plus beau chien du monde 2022 </h1>
+    <?php
+
+    if (have_posts()):
+        while(have_posts()) : the_post(); ?>
+            <h2><?php the_title(); ?></h2>
+        <?php endwhile; ?>
+    <?php endif; ?>    
 <?php get_footer(); ?>
